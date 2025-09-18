597b5abd6a5f8a87e23ba1c7a884455b61677e7f
Miriam <miriamoamohammed@gmail.com>
2025-09-18 15:03:13 +0100

    CHAP1 CONT

---
+++ Introduction to Java Programming and Data Structures/Chapter 1/Exercise1v4.java
@@ -0,0 +1,13 @@
+public class Exercise1v4 {
+    public static void main (String[] args){
+        System.out.println("a   a^2   a^3   a^4");
+
+        for (int a = 1; a <= 4; a++){
+            System.out.println(a + "    " + (int)Math.pow(a, 2) + "    " + (int)Math.pow(a, 3) + "    " + (int)Math.pow(a, 4));
+         
+    }
+
+     
+    }
+    
+}
---
+++ Introduction to Java Programming and Data Structures/Chapter 1/Exercise1v5.java
@@ -0,0 +1,7 @@
+public class Exercise1v5 {
+    public static void main(String[] args) {
+        double answer = (7.5 * 6.5 - 4.5 * 3) / (47.5 - 5.5);
+        System.out.println("The result is: " + answer);
+    }
+
+}
