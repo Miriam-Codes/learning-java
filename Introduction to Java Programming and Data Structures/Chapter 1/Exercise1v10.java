6105dc0b43a770ad9602f546de401a5cb589a69d
Miriam <miriamoamohammed@gmail.com>
2025-09-19 13:59:08 +0100

    Update Exercise1v10.java

--- Introduction to Java Programming and Data Structures/Chapter 1/Exercise1v10.java
+++ Introduction to Java Programming and Data Structures/Chapter 1/Exercise1v10.java
@@ -1,8 +1,12 @@
 public class Exercise1v10 {
     public static void main (String[]args){
-        final double MILETOKILOMETER = 1.6;
+        final double KILOMETERTOMILE = 1.6;
 
         //CONVERT KILOMETER TO MILE
+        double miles = 15 / KILOMETERTOMILE;
+
+        //convert minutes and seconds to hour
+        double time = 
         //SPEED = DISTANCE *TIME
         
     }
